6114b6eaed831c7a09d1914576e4fdc6d062eeb5
Olga_Belaya <protenot@yandex.ru>
2024-01-31 18:56:47 +0300

    add field for solution

--- src/components/ChosenTask.vue
+++ src/components/ChosenTask.vue
@@ -1,21 +1,16 @@
-<template>
-  <div>
-    <button @click="goBack">Назад</button>
-    <h1>{{ localTask?.description }}</h1>
-    <p v-if="localTask">Сложность: {{ localTask?.complexity }}</p>
-    <p v-if="localTask">Язык: {{ localTask?.language }}</p>
-    <p v-if="localTask">Тег: {{ localTask?.tag }}</p>
-  </div>
-</template>
 
-<script lang="ts">
-import { TASKS } from '@/fakeDB/tasks'
-import type { Task } from '@/models/task.model'
-import { ref, onMounted } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
-export default {
-  setup() {
-    const localTask = ref(null as Task | null)
+
+<script setup lang="ts">
+
+import { TASKS } from '@/fakeDB/tasks';
+import type { Task } from '@/models/task.model';
+import {ref,  onMounted } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import Monaco from './Monaco.vue'
+
+    
+const localTask = ref(null as Task | null);
+  
     const route = useRoute()
     const router = useRouter()
     const goBack = () => {
@@ -23,18 +18,44 @@ export default {
     }
 
     const loadTask = (taskId: string) => {
-      localTask.value = TASKS.find((task) => task.id === taskId) || null
+     localTask.value = TASKS.find((task) => task.id === taskId) || null
+      
     }
+
 
     onMounted(() => {
       const taskId = route.params.id as string
       loadTask(taskId)
+      
+  
     })
 
-    return {
-      localTask,
-      goBack
+    function editorChange(val: string) {
+    
+    
+      console.log(val)
     }
-  }
+    
+    function compareSolution() {
+  
 }
+
 </script>
+<template>
+    <div>
+      <button @click="goBack">Назад</button>
+      <h1>{{ localTask?.description }}</h1>
+      <p v-if="localTask">Сложность: {{ localTask?.complexity }}</p>
+      <p v-if="localTask">Язык: {{ localTask?.language }}</p>
+      <p v-if="localTask">Тег: {{ localTask?.tag }}</p>
+      <Monaco
+        ref="monacoEdit"
+        :value="''"
+        :readonly="false"
+        type="curl"
+        :height="300"
+        @editorChange="editorChange"
+      ></Monaco>
+      <button @click="compareSolution">Сравнить с решением</button>  
+   </div>
+  </template>
---
+++ vue-monaco-editor.d.ts
@@ -0,0 +1,4 @@
+declare module 'vue-monaco-editor'
+/* {
+    const VueMonacoEditor: any;
+    export default VueMonacoEditor;} */
