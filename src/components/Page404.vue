3d12b603e89b738bd188a9dbc2751d80d4eb3008
Olga_Belaya <protenot@yandex.ru>
2024-03-09 19:49:13 +0300

    final commit

--- src/components/Page404.vue
+++ src/components/Page404.vue
@@ -1,5 +1,5 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-// eslint-disable-next-line vue/multi-word-component-names
+
 <template>
   <p>Page "{{ route.fullPath }}" not found</p>
   <RouterLink to="/"> На главную </RouterLink>
